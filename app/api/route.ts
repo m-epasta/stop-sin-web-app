57b93fe35c043a2e08621ff95784f57375228c0a
m-epasta <mepastaacc@outlook.com>
2025-10-03 23:09:51 +0200

    Updated route.ts

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,6 +1,19 @@
 import { NextRequest, NextResponse } from 'next/server';
-// export async function POST(request: NextRequest) {
-//     try {
+import { StatsPage, VALID_API_KEYS } from "../stats/page";
 
-//     }
-// }
+const actual_API_keys: string[] = VALID_API_KEYS;
+
+
+export async function GET(request: NextRequest) {
+    var authHeader = request.headers.get('authorization') || request.headers.get('Authorization')
+    try {
+        if (authHeader){
+            
+        }
+    }
+    
+}
+
+function analyzeAuth(request: NextRequest) {
+    var authHeader = request.headers.get('authorization') || request.headers.get('Authorization');
+}
